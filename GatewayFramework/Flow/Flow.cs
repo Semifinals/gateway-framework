3baca1a26f78e299354bcee0cdf54a2db796ed3c
Finley Sherwood <me@itsf.in>
2023-05-08 14:56:41 +1000

    Create handlers to manage common gateway exceptions

--- GatewayFramework/Flow/Flow.cs
+++ GatewayFramework/Flow/Flow.cs
@@ -1,4 +1,5 @@
-﻿using Semifinals.Utils.GatewayFramework.Http;
+﻿using Microsoft.AspNetCore.Mvc;
+using Semifinals.Utils.GatewayFramework.Http;
 
 namespace Semifinals.Utils.GatewayFramework;
 
@@ -8,9 +9,9 @@ public class Flow
 
     public Dictionary<string, HttpResponseMessage>? Responses { get; set; } = null;
 
-    public Flow(Request req)
+    public Flow(Request req, string key = "singular")
     {
-        Requests = new() { { "singular", req } };
+        Requests = new() { { key, req } };
     }
 
     public Flow(Dictionary<string, Request> reqs)
@@ -34,5 +35,41 @@ public class Flow
     {
         Responses = await pipe.Pipe(Requests);
         return this;
+    }
+
+    public static async Task<IActionResult> Handle(
+        Flow flow,
+        Func<Flow, Task<IActionResult>> handler)
+    {
+        try
+        {
+            return await handler(flow);
+        }
+        catch (UnauthenticatedException)
+        {
+            return new UnauthorizedResult();
+        }
+        catch (UnauthorizedException)
+        {
+            return new ForbidResult();
+        }
+        catch (Exception)
+        {
+            return new StatusCodeResult(502);
+        }
+    }
+
+    public static async Task<IActionResult> Handle(
+        Dictionary<string, Request> reqs,
+        Func<Flow, Task<IActionResult>> handler)
+    {
+        return await Handle(new Flow(reqs), handler);
+    }
+
+    public static async Task<IActionResult> Handle(
+        Request req,
+        Func<Flow, Task<IActionResult>> handler)
+    {
+        return await Handle(new Flow(req), handler);
     }
 }
